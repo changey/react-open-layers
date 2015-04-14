5b81b07ab4f6448c8bbce2ede3af53e72398c244
Eric Chang <echang@sunrunhome.com>
2015-04-14 14:03:38 -0700

    can render map:_

--- faecbook-flux/js/app.jsx
+++ faecbook-flux/js/app.jsx
@@ -4,30 +4,54 @@ require('es6-shim');
 require('es6-shim/es6-sham');
 
 var React = require('react');
-var App = require('./components/App.jsx');
 var OLFeature = require('./components/OLFeature')
 var OLMap =require("./components/OLMap")
 
 var MapView = React.createClass({
 
-	getInitialState: function() {
+	getInitialState() {
     return {
         showDialog: true
     };
   },
 
-	create_map: function() {
-    console.log("bar");
+	componentWillMount() {
+    // this.map = new OpenLayers.Map('map', {
+		// 	div: "map"
+		// });
+		var map = new OpenLayers.Map('map', {
+			div: "map",
+			projection: new OpenLayers.Projection("EPSG:900913"),
+			displayProjection: new OpenLayers.Projection("EPSG:4326"),
+			layers: [
+			new OpenLayers.Layer.OSM()
+			]
+		});
+		map.addLayer(new OpenLayers.Layer.OSM());
+		map.setCenter(new OpenLayers.LonLat(0, 0), 2);
+
+		this.setState({map: map});
+    this.map = map;
 	},
 
-  render: function() {
+  componentDidMount() {
+    this.map.render(document.getElementById('map'))
+	},
+
+	componentWillUpdate() {
+		console.log(this.map)
+	},
+
+  render() {
 		return (
 			<div id="something">
-			  <OLMap zoom="4">
-          <OLFeature>
-            bar
-          </OLFeature>
+			  <OLMap id="ol-map" zoom="4"
+					map = {this.map}>
+
         </OLMap>
+				<OLFeature>
+					bar
+				</OLFeature>
       </div>
 		)
   }
--- faecbook-flux/js/components/OLMap.jsx
+++ faecbook-flux/js/components/OLMap.jsx
@@ -17,16 +17,7 @@ var OLMap = React.createClass({
 		//this.map.setTarget(document.getElementById('olMap'));
 		// this.map.renderSync();
 
-		map = new OpenLayers.Map('map', {
-			div: "map",
-			projection: new OpenLayers.Projection("EPSG:900913"),
-			displayProjection: new OpenLayers.Projection("EPSG:4326"),
-			layers: [
-			new OpenLayers.Layer.OSM()
-			]
-		});
-		map.addLayer(new OpenLayers.Layer.OSM());
-		map.setCenter(new OpenLayers.LonLat(0, 0), 2);
+
 		// var map = new OpenLayers.Map("map", {
 		// 	div: "map",
 		// 	controls: []
@@ -42,37 +33,38 @@ var OLMap = React.createClass({
 		// vectors.addFeatures([polygonFeature]);
 		//
 		// map.zoomToExtent(vectors.getDataExtent());
+		console.log(this.props)
 
-		map.addLayer(new OpenLayers.Layer('base', {
-			isBaseLayer: true,
-			maxExtent: bounds
-		}));
-
-		var bounds = new OpenLayers.Bounds(-100, -100, 100, 100);
-
-		// vector1 - the blue marker shape
-		var vectorLayer1 = new OpenLayers.Layer.Vector("vector1", {
-			maxExtent: bounds
-		});
-		map.addLayer(vectorLayer1);
-
-
-		var f1 = new OpenLayers.Feature.Vector(
-			new OpenLayers.Geometry.Point(-50, 50), {
-				id: 'A'
-			}, {
-				externalGraphic: 'http://dev.openlayers.org/releases/OpenLayers-2.13/img/marker-blue.png',
-				graphicWidth: 21,
-				graphicHeight: 25
-			});
-			vectorLayer1.addFeatures(f1);
-
-			var drag1 = new OpenLayers.Control.DragFeature(vectorLayer1, {
-				autoActivate: true
-			});
-			map.addControl(drag1);
-
-			map.zoomToMaxExtent();
+		// map.addLayer(new OpenLayers.Layer('base', {
+		// 	isBaseLayer: true,
+		// 	maxExtent: bounds
+		// }));
+		//
+		// var bounds = new OpenLayers.Bounds(-100, -100, 100, 100);
+		//
+		// // vector1 - the blue marker shape
+		// var vectorLayer1 = new OpenLayers.Layer.Vector("vector1", {
+		// 	maxExtent: bounds
+		// });
+		// map.addLayer(vectorLayer1);
+		//
+		//
+		// var f1 = new OpenLayers.Feature.Vector(
+		// 	new OpenLayers.Geometry.Point(-50, 50), {
+		// 		id: 'A'
+		// 	}, {
+		// 		externalGraphic: '../images/pikachu.png',
+		// 		graphicWidth: 40,
+		// 		graphicHeight: 40
+		// 	});
+		// 	vectorLayer1.addFeatures(f1);
+		//
+		// 	var drag1 = new OpenLayers.Control.DragFeature(vectorLayer1, {
+		// 		autoActivate: true
+		// 	});
+		// 	map.addControl(drag1);
+		//
+		// 	map.zoomToMaxExtent();
 
 			// var dragControl = new OpenLayers.Control.DragFeature(vectors);
 			//
@@ -93,11 +85,3 @@ var OLMap = React.createClass({
 	});
 
 	module.exports = OLMap;
-	// React.renderComponent(
-	// 	    <OLMap
-	//         zoom="4"
-	// 				// cancelButtonText="Cancel"
-	// 				// okAction={this.okAction}
-	// 			/>,
-	// 			document.getElementById('react')
-	// );
