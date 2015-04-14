4c97e7e7399177281cc6832f1038b05d6d00715b
Eric Chang <echang@sunrunhome.com>
2015-04-14 11:44:05 -0700

    added drag feature

--- faecbook-flux/js/components/OLMap.jsx
+++ faecbook-flux/js/components/OLMap.jsx
@@ -27,38 +27,77 @@ var OLMap = React.createClass({
 		});
 		map.addLayer(new OpenLayers.Layer.OSM());
 		map.setCenter(new OpenLayers.LonLat(0, 0), 2);
+		// var map = new OpenLayers.Map("map", {
+		// 	div: "map",
+		// 	controls: []
+		// });
 
-		var wkt = new OpenLayers.Format.WKT();
+		// var wkt = new OpenLayers.Format.WKT();
+		//
+		// var vectors = new OpenLayers.Layer.Vector('My Vectors');
+		// map.addLayer(vectors);
+		//
+		// var polygonFeature = wkt.read("POLYGON((-15 2, -15 -10, 7 -10, 7 2, -15 2))");
+		// polygonFeature.geometry.transform(map.displayProjection, map.getProjectionObject());
+		// vectors.addFeatures([polygonFeature]);
+		//
+		// map.zoomToExtent(vectors.getDataExtent());
 
-		var vectors = new OpenLayers.Layer.Vector('My Vectors');
-        map.addLayer(vectors);
+		map.addLayer(new OpenLayers.Layer('base', {
+			isBaseLayer: true,
+			maxExtent: bounds
+		}));
 
-		var polygonFeature = wkt.read("POLYGON((-15 2, -15 -10, 7 -10, 7 2, -15 2))");
-		polygonFeature.geometry.transform(map.displayProjection, map.getProjectionObject());
-		vectors.addFeatures([polygonFeature]);
+		var bounds = new OpenLayers.Bounds(-100, -100, 100, 100);
 
-		map.zoomToExtent(vectors.getDataExtent());
-	},
+		// vector1 - the blue marker shape
+		var vectorLayer1 = new OpenLayers.Layer.Vector("vector1", {
+			maxExtent: bounds
+		});
+		map.addLayer(vectorLayer1);
 
-	create_map: function() {
-		console.log("bar");
-	},
 
-	render: function() {
-		return (
-			<div id="map">
-				foo
-			</div>
-		)
-	}
-});
+		var f1 = new OpenLayers.Feature.Vector(
+			new OpenLayers.Geometry.Point(-50, 50), {
+				id: 'A'
+			}, {
+				externalGraphic: 'http://dev.openlayers.org/releases/OpenLayers-2.13/img/marker-blue.png',
+				graphicWidth: 21,
+				graphicHeight: 25
+			});
+			vectorLayer1.addFeatures(f1);
 
-module.exports = OLMap;
-// React.renderComponent(
-// 	    <OLMap
-//         zoom="4"
-// 				// cancelButtonText="Cancel"
-// 				// okAction={this.okAction}
-// 			/>,
-// 			document.getElementById('react')
-// );
+			var drag1 = new OpenLayers.Control.DragFeature(vectorLayer1, {
+				autoActivate: true
+			});
+			map.addControl(drag1);
+
+			map.zoomToMaxExtent();
+
+			// var dragControl = new OpenLayers.Control.DragFeature(vectors);
+			//
+			// map.addControl(dragControl);
+		},
+
+		create_map: function() {
+			console.log("bar");
+		},
+
+		render: function() {
+			return (
+				<div id="map">
+					foo
+				</div>
+			)
+		}
+	});
+
+	module.exports = OLMap;
+	// React.renderComponent(
+	// 	    <OLMap
+	//         zoom="4"
+	// 				// cancelButtonText="Cancel"
+	// 				// okAction={this.okAction}
+	// 			/>,
+	// 			document.getElementById('react')
+	// );
